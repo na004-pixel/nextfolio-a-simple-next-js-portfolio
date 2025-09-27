e27a12a0feac6621603e6c506e8465ed1809f3f1
na004-pixel <nonzenzegirl@gmail.com>
2025-09-27 19:41:56 +0530

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -10,18 +10,18 @@ export const projects: Project[] = [
     title: "Vega AI",
     year: 2025,
     description: "AI powered Automation Platform",
-    url: "/blog/vega-overview",
+    url: "/blog/v2-vega-ai",
   },
   {
     title: "Compound Interest Model",
     year: 2025,
     description: "Predict compound interest with high accuracy and speed",
-    url: "/blog/CI-model",
+    url: "/blog/v2-ci-model",
   },
   {
     title: "Automated Backup to S3",
     year: 2025,
     description: "Powerful durable cost-effective hassle-free backups",
-    url: "/blog/s3-backup",
+    url: "/blog/v2-s3-backup",
   },
 ];
