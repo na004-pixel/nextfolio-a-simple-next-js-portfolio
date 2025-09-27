2c555381757dcc13b6c26a46549d3a72316c5316
na004-pixel <nonzenzegirl@gmail.com>
2025-09-27 17:47:47 +0530

    Update config.ts

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,7 +1,7 @@
 export const metaData = {
-  baseUrl: "https://nextfolio-template.vercel.app/",
-  title: "Nextfolio",
-  name: "Sirius",
+  baseUrl: "https://noor-portfolio-v1-alpha.vercel.app/",
+  title: "My Portfolio",
+  name: "Noor",
   ogImage: "/opengraph-image.png",
   description:
     "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
