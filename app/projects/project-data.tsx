14c1564b2f3ad4857060f487bae19425e09fd565
na004-pixel <nonzenzegirl@gmail.com>
2025-09-27 19:10:00 +0530

    Update project-data.tsx

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -8,13 +8,13 @@ export interface Project {
 export const projects: Project[] = [
   {
     title: "Vega AI",
-    year: 2024,
+    year: 2025,
     description: "AI powered Automation Platform",
     url: "/blog/vega-overview",
   },
   {
     title: "Compound Interest Model",
-    year: 2023,
+    year: 2025,
     description: "Predict compound interest with high accuracy and speed",
     url: "/blog/CI-model",
   },
